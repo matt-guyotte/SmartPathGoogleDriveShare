a3d425d2ab94f738bfcd9f8a2169af5b06d4eda9
Matt Guyotte <matt.j.guyotte@gmail.com>
2020-08-23 12:35:39 -0400

    changed token

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const {client_secret, client_id, redirect_uris} = credentials.web;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
@@ -54,8 +54,9 @@ function authorize(credentials, callback) {
  */
 function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
+    access_type: 'online',
     scope: SCOPES,
+    response_type: 'token',
   });
   console.log('Authorize this app by visiting this url:', authUrl);
   const rl = readline.createInterface({
