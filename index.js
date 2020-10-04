d6711f0f6744ed3890a59f56a2115a1dbb124054
Matt Guyotte <matt.j.guyotte@gmail.com>
2020-10-03 20:35:28 -0400

    changed to correct parent path

--- index.js
+++ index.js
@@ -1104,7 +1104,7 @@ app.post('/classroomexport', async (req, res) => {
       var fileMetadata = {
         'name': fileName,
         'description': description,
-        'parents': [parentId]
+        'parents': [parentFolder]
       };
       console.log(fileMetadata)
       var media = {
@@ -1137,7 +1137,7 @@ app.post('/classroomexport', async (req, res) => {
       var fileMetadata = {
         'name': fileName,
         'description': description,
-        'parents': [parentId],
+        'parents': [parentFolder],
         'mimeType': newType,
       };
       drive.files.create({
