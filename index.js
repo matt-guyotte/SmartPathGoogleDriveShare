b197d173cd532401b2bdcd060ac2e15b583b3876
Matt Guyotte <matt.j.guyotte@gmail.com>
2020-10-22 16:24:59 -0400

    n

--- index.js
+++ index.js
@@ -779,7 +779,7 @@ app.get("/drivecall2", (req, res) => {
   // */
   async function listFiles2(auth) {
     const drive = google.drive({ version: "v3", auth });
-    console.log(drive.drive);
+    console.log(drive);
     app.set("drive2", drive)
     const response = await drive.files.list({
       pageSize: 1000,
