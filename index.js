49fd3b1476093beac11c52d63197209a50d7a5a9
Matt Guyotte <matt.j.guyotte@gmail.com>
2021-02-23 17:39:58 -0500

    n

--- index.js
+++ index.js
@@ -578,7 +578,7 @@ app.post("/downloaddocument", async (req, res) => {
         newType === 'application/pdf'
       }
 
-      const dest = await fs.createWriteStream('./src/Pages/downloads/' + fileName + '.' + type, (err) => {if (err) return console.log(err)});
+      const dest = await fs.createWriteStream('./src/Pages/downloads/' + fileName + '.' + type);
       const destSimple = './src/Pages/downloads/' + fileName + '.' + type;
       //console.log(destSimple)
 
