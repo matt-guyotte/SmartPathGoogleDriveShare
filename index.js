7aa9028cf118cc4aa346243d78906c18b060d6ee
Matt Guyotte <matt.j.guyotte@gmail.com>
2021-03-16 16:32:11 -0400

    'n'

--- index.js
+++ index.js
@@ -658,7 +658,7 @@ app.post("/downloaddocument", async (req, res) => {
     }
 
     if(files[i].type === "folder") {
-      const mkDir1 = fs.mkdir('./src/Pages/downloads' + files[i].name, { recursive: true }, (err) => {
+      const mkDir1 = await fs.mkdir('./src/Pages/downloads' + files[i].name, { recursive: true }, (err) => {
         if (err) return console.log("At 764: " + err);
         console.log("directory 1 made.")
       })
